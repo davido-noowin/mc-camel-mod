3c90e74492d69322d992b5fb06fea5fdc3ae1867
David Nguyen <davido.nguyen1212@gmail.com>
2025-04-25 16:36:42 -0700

    fabric mod template

---
+++ src/client/java/davido/noowin/evan/mod/EvanModClient.java
@@ -0,0 +1,10 @@
+package davido.noowin.evan.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EvanModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
